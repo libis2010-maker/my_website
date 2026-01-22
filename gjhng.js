119efe25fb7a3d5fdfec62d9349d84dedc72083c
libis2010-maker <libis2010@ofekschool.org.il>
2026-01-22 14:26:59 +0200

    Add files via upload

---
+++ gjhng.js
@@ -0,0 +1,8 @@
+let p=document.querySelector(".main")
+ function logout(){
+    p.style.color="pink"
+    p.style.backgroundColor="green";
+    p.style.border="1px solid";
+    p.style.borderColor="black";
+    p.style.borderRadius="10px";
+ }
